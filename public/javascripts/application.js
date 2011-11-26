0f550ae3b6f517d8c75f283bd0f11180af1f711e
Ankit Ahuja <ahuja.ankit@gmail.com>
2011-11-26 00:28:26 -0500

    Minor css tweaks

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,13 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(e) {
   $(".topbar").dropdown();
-  $(".like-count, .comment-count, .like-btn, .topic-keyword").twipsy({
+  $(".like-count, .comment-count, .like-btn").twipsy({
     animate: false,
     delayIn: 10
+  });
+  $(".topic-keyword").twipsy({
+    animate: false,
+    delayIn: 1000
   });
   $(".story-liker, .avatar").twipsy({
     animate: false,
