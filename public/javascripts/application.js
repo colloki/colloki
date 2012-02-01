e03330bf95bf5612ba440f4c631b6f31285a8a8c
Ankit Ahuja <ahuja.ankit@gmail.com>
2012-02-01 12:03:39 -0500

    upgrading to boostrap v2

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,19 +2,19 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(e) {
   $(".topbar").dropdown();
-  $(".like-count, .comment-count, .like-btn").twipsy({
-    animate: false,
-    delayIn: 10
-  });
-  $(".topic-keyword").twipsy({
-    animate: false,
-    delayIn: 1000
-  });
-  $(".story-liker, .avatar").twipsy({
-    animate: false,
-    delayIn: 10,
-    placement: 'below'
-  });
-  $(".alert-message").alert();
-  $('.tabs').tabs();
+  // $(".like-count, .comment-count, .like-btn").twipsy({
+  //   animate: false,
+  //   delayIn: 10
+  // });
+  // $(".topic-keyword").twipsy({
+  //   animate: false,
+  //   delayIn: 1000
+  // });
+  // $(".story-liker, .avatar").twipsy({
+  //   animate: false,
+  //   delayIn: 10,
+  //   placement: 'below'
+  // });
+  // $(".alert-message").alert();
+  // $('.tabs').tabs();
 });
