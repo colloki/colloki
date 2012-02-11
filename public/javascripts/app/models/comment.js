34741bec4054cf2994bb6a56eb6283678945ddb4
Ankit Ahuja <ahuja.ankit@gmail.com>
2012-02-11 17:10:42 -0500

    Fixed paths for voting and comments in backbone

--- public/javascripts/app/models/comment.js
+++ public/javascripts/app/models/comment.js
@@ -1,6 +1,6 @@
 $(function() {
   window.Comment = Backbone.Model.extend({
-    rootUrl: '/comments',
+    rootUrl: APP_ROOT_PATH + 'comments',
 
     validate: function(attrs) {
       if (attrs.body === "") {
