6df456d9e12482f3406507ae09013e0f67f351fa
Ankit Ahuja <ahuja.ankit@gmail.com>
2012-02-15 10:14:21 -0500

    Added popovers to topics in navigation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(e) {
+  
   $(".topbar").dropdown();
+  $(".has_popover").popover({
+    placement: 'bottom',
+    delay: 500
+  });
+
   // $(".like-count, .comment-count, .like-btn").twipsy({
   //   animate: false,
   //   delayIn: 10
