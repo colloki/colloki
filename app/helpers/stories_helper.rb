cd02b730f8e988759f4caa351b5f735df76526b3
Ankit Ahuja <ahuja.ankit@gmail.com>
2012-03-05 18:44:51 -0500

    Added share button to nav bar

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -12,35 +12,6 @@ module StoriesHelper
     html.html_safe
   end
 
-  def story_edit_btn(story)
-    html = "&nbsp;"
-    html << "<div class='btn-group'>"
-    html << "<a class='btn btn-info btn-small' href='#'>Edit</a>"
-    html << "<a class='btn btn-info btn-small dropdown-toggle' data-toggle='dropdown' href='#'>"
-    html << "<span class='caret'></span>"
-    html << "</a>"
-    html << "<ul class='dropdown-menu'>"
-    html << "<li>"
-    html << "<a data-toggle='modal' href='#delete_confirm_" << story.id.to_s << "'>Delete</a>"
-    html << "</li>"
-    html << "</ul>"
-    html << "</div>"
-    html << "<div class='modal' style='display:none;' id='delete_confirm_" << story.id.to_s << "'>"
-    html << "<div class='modal-header'>"
-    html << "<a class='close' data-dismiss='modal'>&times;</a>"
-    html << "<h3>Are you sure?</h3>"
-    html << "</div>"
-    html << "<div class='modal-body'>"
-    html << "<p>You want to delete '" << story.title << "'</p>"
-    html << "</div>"
-    html << "<div class='modal-footer'>"
-    html << "<a href='#' class='btn' data-dismiss='modal'>Cancel</a>"
-    html << (link_to "Ok", story, :method => :delete, :class => 'btn btn-danger')
-    html << "</div>"
-    html << "</div>"
-    html.html_safe
-  end
-
   def sidebar_story_image(story)
     if image_exists? story.image_file_name
       return link_to image_tag(story.image.url(:thumb), :class=>'sidebar-story-thumb'), story
